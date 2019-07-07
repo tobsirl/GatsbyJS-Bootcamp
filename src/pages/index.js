ffcb12c565d4b86c9de0ba364b0a10019966ca06
Paul Tobin <tobster111@gmail.com>
2019-07-07 09:35:39 +0100

    Gatsby Bootcamp

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,15 @@
-import React from "react"
+import React from "react";
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <h2>I'm Paul,</h2>
+      <p>
+        Need a developer? <a href="/contact">Contact me</a>
+      </p>
+    </div>
+  );
+};
+
+export default IndexPage;
