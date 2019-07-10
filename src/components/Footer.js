55901c9cce105456ed16dcbde2accea338549894
Paul Tobin <tobster111@gmail.com>
2019-07-10 15:01:48 +0100

    Using GraphQL is populate the site with dynamic data

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
 
 const Footer = () => {
+  const data = useStaticQuery(graphql`
+    {
+      site {
+        siteMetadata {
+          author
+        }
+      }
+    }
+  `);
+
+  const { author } = data.site.siteMetadata;
+
   return (
     <footer>
-      <p>Created by Paul Tobin, </p>
+      <p>Created by {author}, &copy; 2019</p>
     </footer>
   );
 };
