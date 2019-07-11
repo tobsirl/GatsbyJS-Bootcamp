6a6c8ab1ff23771619837ca423cc96b528edf64e
Paul Tobin <tobster111@gmail.com>
2019-07-11 19:47:16 +0100

    Add a templates directory with a Blog.js component

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,6 +5,11 @@ module.exports.onCreateNode = ({ node, actions }) => {
 
   if (node.internal.type === 'MarkdownRemark') {
     const slug = path.basename(node.fileAbsolutePath, '.md');
-    console.log('@@@@@@@@@@@@@@@@@@@@@@@@@', slug);
+
+    createNodeField({
+      node,
+      name: 'slug',
+      value: slug,
+    });
   }
 };
---
+++ src/templates/Blog.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Layout from '../components/layout/layout';
+
+const Blog = () => {
+  return <Layout>This is a blog template</Layout>;
+};
+
+export default Blog;
