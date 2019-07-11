de75e348cd846cf7c0867c37e26d0afbefa4e24d
Paul Tobin <tobster111@gmail.com>
2019-07-11 12:19:03 +0100

    Installed gatsby-transformer-remark, made a query to the data using grapql and displayed the data in the react blot component using map()

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,40 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
 
 import Layout from '../components/layout/layout';
 
 const BlogPage = () => {
+  const posts = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+            html
+            excerpt
+          }
+        }
+      }
+    }
+  `);
+
   return (
     <Layout>
       <h1>Blog</h1>
       <p>Post will show up here later on</p>
+      <ol>
+        {posts.allMarkdownRemark.edges.map(post => {
+          return (
+            <li key={post.node.frontmatter.date}>
+              <h2>{post.node.frontmatter.title}</h2>
+              <p>{post.node.frontmatter.date}</p>
+            </li>
+          );
+        })}
+      </ol>
     </Layout>
   );
 };
