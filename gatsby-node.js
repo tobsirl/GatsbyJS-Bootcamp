7d0d04295024d9287af4ee2b47b1165135148dad
Paul Tobin <tobster111@gmail.com>
2019-07-11 19:11:30 +0100

    Using the node.js path method basename to get the slug

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,10 @@
+const path = require('path');
+
 module.exports.onCreateNode = ({ node, actions }) => {
   const { createNodeField } = actions;
 
   if (node.internal.type === 'MarkdownRemark') {
-    console.log(JSON.stringify(node, undefined, 4));
+    const slug = path.basename(node.fileAbsolutePath, '.md');
+    console.log('@@@@@@@@@@@@@@@@@@@@@@@@@', slug);
   }
 };
