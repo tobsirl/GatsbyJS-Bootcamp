d6ea80d6a8308faece26d309f3e22fc55c32090e
Paul Tobin <tobster111@gmail.com>
2019-07-11 20:00:33 +0100

    Use createPages to dynamically create the blog posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -13,3 +13,36 @@ module.exports.onCreateNode = ({ node, actions }) => {
     });
   }
 };
+
+module.exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+
+  // Get path to template
+  const blogTemplate = path.resolve('./src/templates/Blog.js');
+
+  // Get the markdown for the blog
+  const res = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  // Create the new pages
+  res.data.allMarkdownRemark.edges.forEach(edge => {
+    createPage({
+      component: blogTemplate,
+      path: `/blog/${edge.node.fields.slug}`,
+      context: {
+        slug: edge.node.fields.slug,
+      },
+    });
+  });
+};
