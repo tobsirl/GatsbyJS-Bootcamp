b1abea159527c11690c03379edfbe71b5c7311e3
Paul Tobin <tobster111@gmail.com>
2019-07-12 11:32:44 +0100

    Added a graphql query to the Blog template component to dynamically render the blog page

--- src/templates/Blog.js
+++ src/templates/Blog.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
 import Layout from '../components/layout/layout';
 
-const Blog = () => {
-  return <Layout>This is a blog template</Layout>;
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        date
+      }
+      html
+    }
+  }
+`;
+
+const Blog = props => {
+  return (
+    <Layout>
+      <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+    </Layout>
+  );
 };
 
 export default Blog;
