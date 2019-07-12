7323cbe64e02aae00d0d558013e20418b4718ae8
Paul Tobin <tobster111@gmail.com>
2019-07-12 12:05:58 +0100

    Styling the footer component with modules scss

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 
+import footerStyles from './Footer.module.scss';
+
 const Footer = () => {
   const data = useStaticQuery(graphql`
     {
@@ -15,7 +17,7 @@ const Footer = () => {
   const { author } = data.site.siteMetadata;
 
   return (
-    <footer>
+    <footer className={footerStyles.footer}>
       <p>Created by {author}, &copy; 2019</p>
     </footer>
   );
