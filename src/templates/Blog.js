482ea73a7973efec3285c26de51429c4095144bf
Paul Tobin <tobster111@gmail.com>
2019-07-13 15:21:53 +0100

    Installed @contentful/rich-text-react-renderer to make the json document easier to handle

--- src/templates/Blog.js
+++ src/templates/Blog.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 import Layout from '../components/layout/layout';
 
